95699742df616096089f64b7976eca98b23dfa1c
Barath Vikraman M <136318274+M-Barath-Vikraman@users.noreply.github.com>
2024-10-15 11:44:36 +0530

    Time: 4 ms (85.43%), Space: 8.5 MB (75.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+
+        long rev=0;
+        long temp=x;
+
+        while(temp !=0){
+            int digit = temp % 10;
+            rev = rev *10+digit;
+            temp = temp/10;
+        }
+        return (rev == x);
+    }
+};
