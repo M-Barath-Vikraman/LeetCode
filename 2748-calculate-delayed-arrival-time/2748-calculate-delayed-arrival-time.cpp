75a84c664a4555e607686052275058669f467801
Barath Vikraman M <136318274+M-Barath-Vikraman@users.noreply.github.com>
2024-12-16 11:49:51 +0530

    Time: 0 ms (100%), Space: 8.1 MB (29.66%) - LeetHub

--- 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.cpp
+++ 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
-        int t=arrivalTime + delayedTime;
-        if( t==24){
-            return 0;
-        }
-        else if(t>24){
-            return t-24;
-        }
-        else{return t;}
+        // int t=arrivalTime + delayedTime;
+        // if( t==24){
+        //     return 0;
+        // }
+        // else if(t>24){
+        //     return t-24;
+        // }
+        // else{return t;
+        return (arrivalTime+delayedTime)%24;
     }
 };
