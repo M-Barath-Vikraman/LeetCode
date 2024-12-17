f36a8f056f7fc30e1b6d3c2b33641516c63bc286
Barath Vikraman M <136318274+M-Barath-Vikraman@users.noreply.github.com>
2024-12-17 22:58:29 +0530

    Time: 0 ms (100%), Space: 10 MB (7.19%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> stack;
+        for(int i=0;i<=s.size()-1;i++){
+            if(s[i] == '(' || s[i] =='[' ||s[i] == '{'){
+                stack.push(s[i]);
+            }
+            else{
+                if(stack.empty()||(s[i]==')'&&stack.top() !='(')||
+                (s[i]=='}'&&stack.top() !='{')||(s[i]==']'&&stack.top() !='[')){
+                    return false;
+                }
+                stack.pop();
+            }
+    }
+    return stack.empty();
+    }
+};
